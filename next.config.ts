14a22f9224aec32abd0775fc6be6fdd5bf7b9e8b
iresharma <iresh.sharma8@gmail.com>
2025-09-10 03:16:42 +0530

    fixed now

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      '/': ['./schema.sql', './scripts/**/*', './src/lib/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/': ['./schema.sql', './scripts/**/*', './src/lib/**/*'],
   },
 };
 
